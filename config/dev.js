bf09a541d608c5134249405eccb109fb39a04a48
Reem Krauss <reem.krauss@gmail.com>
2022-06-07 15:02:08 +0300

    day 7/6

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
-// module.exports = {
-//   dbURL: 'mongodb://localhost:27017',
-// }
-const atlas = require('./password')
 module.exports = {
-  dbURL: `mongodb+srv://UpBeat:${atlas.password}@cluster0.wlxkri6.mongodb.net/?retryWrites=true&w=majority`,
+  dbURL: 'mongodb://localhost:27017',
 }
+// const atlas = require('./password')
+// module.exports = {
+//   dbURL: `mongodb+srv://UpBeat:${atlas.password}@cluster0.wlxkri6.mongodb.net/?retryWrites=true&w=majority`,
+// }
